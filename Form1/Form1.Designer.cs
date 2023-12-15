e5a6d3dab45ded03a64bcd12e89b48dfd15958dd
Phung211058 <140879559+Phung211058@users.noreply.github.com>
2023-12-15 17:39:14 +0700

    commit 3

--- Form1/Form1.Designer.cs
+++ Form1/Form1.Designer.cs
@@ -29,29 +29,29 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.rad_mango = new System.Windows.Forms.RadioButton();
+            this.rad_orange = new System.Windows.Forms.RadioButton();
+            this.rad_water = new System.Windows.Forms.RadioButton();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.rad_juice = new System.Windows.Forms.RadioButton();
+            this.rad_smoothie = new System.Windows.Forms.RadioButton();
             this.pgress_cutter = new System.Windows.Forms.ProgressBar();
             this.pgress_cleanser = new System.Windows.Forms.ProgressBar();
             this.pgress_presser = new System.Windows.Forms.ProgressBar();
             this.pgress_blender = new System.Windows.Forms.ProgressBar();
             this.btn_success = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.rad_water = new System.Windows.Forms.RadioButton();
-            this.rad_orange = new System.Windows.Forms.RadioButton();
-            this.rad_mango = new System.Windows.Forms.RadioButton();
-            this.rad_smoothie = new System.Windows.Forms.RadioButton();
-            this.rad_juice = new System.Windows.Forms.RadioButton();
             this.pgress_filter = new System.Windows.Forms.ProgressBar();
             this.lbl_percent_cutter = new System.Windows.Forms.Label();
             this.lbl_percent_blender = new System.Windows.Forms.Label();
             this.lbl_percent_cleanser = new System.Windows.Forms.Label();
             this.lbl_percent_presser = new System.Windows.Forms.Label();
             this.lbl_percent_filter = new System.Windows.Forms.Label();
+            this.lbl_announce = new System.Windows.Forms.Label();
             this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
             this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
             this.backgroundWorker5 = new System.ComponentModel.BackgroundWorker();
-            this.lbl_announce = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -66,6 +66,39 @@
             this.panel1.Size = new System.Drawing.Size(465, 412);
             this.panel1.TabIndex = 0;
             // 
+            // rad_mango
+            // 
+            this.rad_mango.AutoSize = true;
+            this.rad_mango.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad_mango.Location = new System.Drawing.Point(262, 195);
+            this.rad_mango.Name = "rad_mango";
+            this.rad_mango.Size = new System.Drawing.Size(103, 29);
+            this.rad_mango.TabIndex = 2;
+            this.rad_mango.Text = "Mango";
+            this.rad_mango.UseVisualStyleBackColor = true;
+            // 
+            // rad_orange
+            // 
+            this.rad_orange.AutoSize = true;
+            this.rad_orange.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad_orange.Location = new System.Drawing.Point(262, 115);
+            this.rad_orange.Name = "rad_orange";
+            this.rad_orange.Size = new System.Drawing.Size(105, 29);
+            this.rad_orange.TabIndex = 1;
+            this.rad_orange.Text = "Orange";
+            this.rad_orange.UseVisualStyleBackColor = true;
+            // 
+            // rad_water
+            // 
+            this.rad_water.AutoSize = true;
+            this.rad_water.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad_water.Location = new System.Drawing.Point(262, 33);
+            this.rad_water.Name = "rad_water";
+            this.rad_water.Size = new System.Drawing.Size(165, 29);
+            this.rad_water.TabIndex = 0;
+            this.rad_water.Text = "WaterLemon";
+            this.rad_water.UseVisualStyleBackColor = true;
+            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.rad_juice);
@@ -75,16 +108,36 @@
             this.panel2.Size = new System.Drawing.Size(465, 268);
             this.panel2.TabIndex = 1;
             // 
+            // rad_juice
+            // 
+            this.rad_juice.AutoSize = true;
+            this.rad_juice.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad_juice.Location = new System.Drawing.Point(262, 169);
+            this.rad_juice.Name = "rad_juice";
+            this.rad_juice.Size = new System.Drawing.Size(89, 29);
+            this.rad_juice.TabIndex = 1;
+            this.rad_juice.Text = "Juice";
+            this.rad_juice.UseVisualStyleBackColor = true;
+            // 
+            // rad_smoothie
+            // 
+            this.rad_smoothie.AutoSize = true;
+            this.rad_smoothie.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad_smoothie.Location = new System.Drawing.Point(262, 76);
+            this.rad_smoothie.Name = "rad_smoothie";
+            this.rad_smoothie.Size = new System.Drawing.Size(128, 29);
+            this.rad_smoothie.TabIndex = 0;
+            this.rad_smoothie.Text = "Smoothie";
+            this.rad_smoothie.UseVisualStyleBackColor = true;
+            // 
             // pgress_cutter
             // 
             this.pgress_cutter.BackColor = System.Drawing.SystemColors.Control;
             this.pgress_cutter.ForeColor = System.Drawing.Color.IndianRed;
             this.pgress_cutter.Location = new System.Drawing.Point(505, 55);
-            this.pgress_cutter.Maximum = 1000;
             this.pgress_cutter.Name = "pgress_cutter";
             this.pgress_cutter.Size = new System.Drawing.Size(341, 40);
             this.pgress_cutter.TabIndex = 2;
-            this.pgress_cutter.Value = 1000;
             // 
             // pgress_cleanser
             // 
@@ -119,64 +172,10 @@
             // 
             // backgroundWorker1
             // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
             this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
-            // 
-            // rad_water
-            // 
-            this.rad_water.AutoSize = true;
-            this.rad_water.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rad_water.Location = new System.Drawing.Point(262, 33);
-            this.rad_water.Name = "rad_water";
-            this.rad_water.Size = new System.Drawing.Size(165, 29);
-            this.rad_water.TabIndex = 0;
-            this.rad_water.Text = "WaterLemon";
-            this.rad_water.UseVisualStyleBackColor = true;
-            // 
-            // rad_orange
-            // 
-            this.rad_orange.AutoSize = true;
-            this.rad_orange.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rad_orange.Location = new System.Drawing.Point(262, 115);
-            this.rad_orange.Name = "rad_orange";
-            this.rad_orange.Size = new System.Drawing.Size(105, 29);
-            this.rad_orange.TabIndex = 1;
-            this.rad_orange.Text = "Orange";
-            this.rad_orange.UseVisualStyleBackColor = true;
-            // 
-            // rad_mango
-            // 
-            this.rad_mango.AutoSize = true;
-            this.rad_mango.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rad_mango.Location = new System.Drawing.Point(262, 195);
-            this.rad_mango.Name = "rad_mango";
-            this.rad_mango.Size = new System.Drawing.Size(103, 29);
-            this.rad_mango.TabIndex = 2;
-            this.rad_mango.Text = "Mango";
-            this.rad_mango.UseVisualStyleBackColor = true;
-            // 
-            // rad_smoothie
-            // 
-            this.rad_smoothie.AutoSize = true;
-            this.rad_smoothie.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rad_smoothie.Location = new System.Drawing.Point(262, 76);
-            this.rad_smoothie.Name = "rad_smoothie";
-            this.rad_smoothie.Size = new System.Drawing.Size(128, 29);
-            this.rad_smoothie.TabIndex = 0;
-            this.rad_smoothie.Text = "Smoothie";
-            this.rad_smoothie.UseVisualStyleBackColor = true;
-            // 
-            // rad_juice
-            // 
-            this.rad_juice.AutoSize = true;
-            this.rad_juice.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rad_juice.Location = new System.Drawing.Point(262, 169);
-            this.rad_juice.Name = "rad_juice";
-            this.rad_juice.Size = new System.Drawing.Size(89, 29);
-            this.rad_juice.TabIndex = 1;
-            this.rad_juice.Text = "Juice";
-            this.rad_juice.UseVisualStyleBackColor = true;
             // 
             // pgress_filter
             // 
@@ -191,9 +190,8 @@
             this.lbl_percent_cutter.Font = new System.Drawing.Font("Myanmar Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_percent_cutter.Location = new System.Drawing.Point(890, 59);
             this.lbl_percent_cutter.Name = "lbl_percent_cutter";
-            this.lbl_percent_cutter.Size = new System.Drawing.Size(73, 36);
+            this.lbl_percent_cutter.Size = new System.Drawing.Size(0, 36);
             this.lbl_percent_cutter.TabIndex = 8;
-            this.lbl_percent_cutter.Text = "label1";
             // 
             // lbl_percent_blender
             // 
@@ -201,9 +199,8 @@
             this.lbl_percent_blender.Font = new System.Drawing.Font("Myanmar Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_percent_blender.Location = new System.Drawing.Point(890, 142);
             this.lbl_percent_blender.Name = "lbl_percent_blender";
-            this.lbl_percent_blender.Size = new System.Drawing.Size(73, 36);
+            this.lbl_percent_blender.Size = new System.Drawing.Size(0, 36);
             this.lbl_percent_blender.TabIndex = 9;
-            this.lbl_percent_blender.Text = "label2";
             // 
             // lbl_percent_cleanser
             // 
@@ -211,9 +208,8 @@
             this.lbl_percent_cleanser.Font = new System.Drawing.Font("Myanmar Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_percent_cleanser.Location = new System.Drawing.Point(890, 230);
             this.lbl_percent_cleanser.Name = "lbl_percent_cleanser";
-            this.lbl_percent_cleanser.Size = new System.Drawing.Size(73, 36);
+            this.lbl_percent_cleanser.Size = new System.Drawing.Size(0, 36);
             this.lbl_percent_cleanser.TabIndex = 10;
-            this.lbl_percent_cleanser.Text = "label3";
             // 
             // lbl_percent_presser
             // 
@@ -221,9 +217,8 @@
             this.lbl_percent_presser.Font = new System.Drawing.Font("Myanmar Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_percent_presser.Location = new System.Drawing.Point(890, 318);
             this.lbl_percent_presser.Name = "lbl_percent_presser";
-            this.lbl_percent_presser.Size = new System.Drawing.Size(73, 36);
+            this.lbl_percent_presser.Size = new System.Drawing.Size(0, 36);
             this.lbl_percent_presser.TabIndex = 11;
-            this.lbl_percent_presser.Text = "label4";
             // 
             // lbl_percent_filter
             // 
@@ -231,9 +226,8 @@
             this.lbl_percent_filter.Font = new System.Drawing.Font("Myanmar Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_percent_filter.Location = new System.Drawing.Point(890, 406);
             this.lbl_percent_filter.Name = "lbl_percent_filter";
-            this.lbl_percent_filter.Size = new System.Drawing.Size(73, 36);
+            this.lbl_percent_filter.Size = new System.Drawing.Size(0, 36);
             this.lbl_percent_filter.TabIndex = 12;
-            this.lbl_percent_filter.Text = "label5";
             // 
             // lbl_announce
             // 
@@ -244,6 +238,34 @@
             this.lbl_announce.Size = new System.Drawing.Size(307, 36);
             this.lbl_announce.TabIndex = 13;
             this.lbl_announce.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.WorkerReportsProgress = true;
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            this.backgroundWorker3.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker3_ProgressChanged);
+            this.backgroundWorker3.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker3_RunWorkerCompleted);
+            // 
+            // backgroundWorker4
+            // 
+            this.backgroundWorker4.WorkerReportsProgress = true;
+            this.backgroundWorker4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker4_DoWork);
+            this.backgroundWorker4.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker4_ProgressChanged);
+            this.backgroundWorker4.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker4_RunWorkerCompleted);
+            // 
+            // backgroundWorker5
+            // 
+            this.backgroundWorker5.WorkerReportsProgress = true;
+            this.backgroundWorker5.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker5_DoWork);
+            this.backgroundWorker5.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker5_ProgressChanged);
+            this.backgroundWorker5.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker5_RunWorkerCompleted);
             // 
             // Form1
             // 
@@ -297,11 +319,11 @@
         private System.Windows.Forms.Label lbl_percent_cleanser;
         private System.Windows.Forms.Label lbl_percent_presser;
         private System.Windows.Forms.Label lbl_percent_filter;
+        private System.Windows.Forms.Label lbl_announce;
         private System.ComponentModel.BackgroundWorker backgroundWorker2;
         private System.ComponentModel.BackgroundWorker backgroundWorker3;
         private System.ComponentModel.BackgroundWorker backgroundWorker4;
         private System.ComponentModel.BackgroundWorker backgroundWorker5;
-        private System.Windows.Forms.Label lbl_announce;
     }
 }
 
--- Form1/Form1.cs
+++ Form1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DrinkMachineLibrary;
@@ -15,7 +16,10 @@ namespace Form1
 {
     public partial class Form1 : Form
     {
-        
+        //private bool isWorker1Running = false;
+        //private bool isWorker2Running = false;
+        //private bool isWorker3Running = false;
+        //private bool isWorker4Running = false;
         public Form1()
         {
             InitializeComponent();
@@ -38,47 +42,275 @@ namespace Form1
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (rad_water.Checked && rad_smoothie.Checked)
+                {
+                    Thread.Sleep(30);
+                }
+                else if (rad_water.Checked && rad_juice.Checked)
+                {
+                    Thread.Sleep(120);
+                }
+                sum += i;
+                backgroundWorker1.ReportProgress(i);
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    backgroundWorker1.ReportProgress(0);
+                    return;
+                }
+            }
+            e.Result = sum;
         }
 
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
+            pgress_cutter.Value = e.ProgressPercentage;
+            lbl_percent_cutter.Text = e.ProgressPercentage.ToString() + "%";
 
         }
 
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-
+            if (e.Cancelled)
+            {
+                lbl_announce.Text = "Progress has been cancelled";
+            }
+            else if (e.Error != null)
+            {
+                lbl_announce.Text = e.Error.Message;
+            }
+            else
+            {
+                //isWorker1Running = false;
+                //lbl_announce.Text = "Successfully";
+                //isWorker2Running = true;
+                backgroundWorker2.RunWorkerAsync();
+            }
         }
 
         private void btn_ok_Click_1(object sender, EventArgs e)
         {
-            if (!rad_water.Checked && !rad_orange.Checked && !rad_mango.Checked)
+            if (!rad_water.Checked && !rad_orange.Checked && !rad_mango.Checked) 
             {
                 lbl_announce.Text = "Please choose your option!";
             }
+            //else if ((rad_water.Checked || rad_orange.Checked || rad_mango.Checked) && (rad_juice.Checked || rad_smoothie.Checked))
             else
             {
-                lbl_announce.Text = "Wait a few minutes";
+                lbl_announce.Text = "Wait a few minutes, please!";
                 DrinkMachine may = new DrinkMachine();
                 if (rad_water.Checked && rad_smoothie.Checked)
                 {
                     may.MakeWatermelonSmoothie1();
+                    backgroundWorker1.RunWorkerAsync();
                 }
                 else if (rad_water.Checked && rad_juice.Checked)
                 {
                     may.MakeWatermelonJuice();
+                    backgroundWorker1.RunWorkerAsync();
                 }
                 else if (rad_orange.Checked && rad_smoothie.Checked)
                 {
                     may.MakeOrangeSmoothie();
+
                 }
                 else if (rad_orange.Checked && rad_juice.Checked)
                 {
                     may.MakeOrangeJuice();
+
                 }
             }
         }
-           
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (rad_water.Checked && rad_smoothie.Checked)
+                {
+                    Thread.Sleep(30);
+                }
+                else if (rad_water.Checked && rad_juice.Checked)
+                {
+                    Thread.Sleep(120);
+                }
+                sum += i;
+                backgroundWorker2.ReportProgress(i);
+                if (backgroundWorker2.CancellationPending)
+                {
+                    e.Cancel = true;
+                    backgroundWorker2.ReportProgress(0);
+                    return;
+                }
+            }
+            e.Result = sum;
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgress_blender.Value = e.ProgressPercentage;
+            lbl_percent_blender.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lbl_announce.Text = "Progress has been cancelled";
+            }
+            else if (e.Error != null)
+            {
+                lbl_announce.Text = e.Error.Message;
+            }
+            else
+            {
+                backgroundWorker3.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (rad_water.Checked && rad_smoothie.Checked)
+                {
+                    Thread.Sleep(30);
+                }
+                else if (rad_water.Checked && rad_juice.Checked)
+                {
+                    Thread.Sleep(120);
+                }
+                sum += i;
+                backgroundWorker3.ReportProgress(i);
+                if (backgroundWorker3.CancellationPending)
+                {
+                    e.Cancel = true;
+                    backgroundWorker3.ReportProgress(0);
+                    return;
+                }
+            }
+            e.Result = sum;
+        }
+
+        private void backgroundWorker3_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgress_cleanser.Value = e.ProgressPercentage;
+            lbl_percent_cleanser.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void backgroundWorker3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lbl_announce.Text = "Progress has been cancelled";
+            }
+            else if (e.Error != null)
+            {
+                lbl_announce.Text = e.Error.Message;
+            }
+            else
+            {
+                backgroundWorker4.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (rad_water.Checked && rad_smoothie.Checked)
+                {
+                    Thread.Sleep(30);
+                }
+                else if (rad_water.Checked && rad_juice.Checked)
+                {
+                    Thread.Sleep(120);
+                }
+                sum += i;
+                backgroundWorker4.ReportProgress(i);
+                if (backgroundWorker4.CancellationPending)
+                {
+                    e.Cancel = true;
+                    backgroundWorker4.ReportProgress(0);
+                    return;
+                }
+            }
+            e.Result = sum;
+        }
+
+        private void backgroundWorker4_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgress_presser.Value = e.ProgressPercentage;
+            lbl_percent_presser.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void backgroundWorker4_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lbl_announce.Text = "Progress has been cancelled";
+            }
+            else if (e.Error != null)
+            {
+                lbl_announce.Text = e.Error.Message;
+            }
+            else
+            {
+                backgroundWorker5.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker5_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (rad_water.Checked && rad_smoothie.Checked)
+                {
+                    Thread.Sleep(30);
+                }
+                else if (rad_water.Checked && rad_juice.Checked)
+                {
+                    Thread.Sleep(120);
+                }
+                sum += i;
+                backgroundWorker5.ReportProgress(i);
+                if (backgroundWorker5.CancellationPending)
+                {
+                    e.Cancel = true;
+                    backgroundWorker5.ReportProgress(0);
+                    return;
+                }
+            }
+            e.Result = sum;
+        }
+
+        private void backgroundWorker5_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgress_filter.Value = e.ProgressPercentage;
+            lbl_percent_filter.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void backgroundWorker5_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lbl_announce.Text = "Progress has been cancelled";
+            }
+            else if (e.Error != null)
+            {
+                lbl_announce.Text = e.Error.Message;
+            }
+            else
+            {
+                lbl_announce.Text = "Successfully";
+            }
+        }
     }
 }
